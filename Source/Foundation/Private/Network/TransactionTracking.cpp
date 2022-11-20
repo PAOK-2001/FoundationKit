1950bed17e3be4cc317a58c4dca6a5d4841d6424
Enrique Rivera <erivera7240@gmail.com>
2022-11-19 20:04:38 -0600

    added the transaction count things

--- Source/Foundation/Private/Network/TransactionTracking.cpp
+++ Source/Foundation/Private/Network/TransactionTracking.cpp
@@ -8,20 +8,49 @@ static FText ErrorMessage = FText::FromString("Error");
 static FText InfoMessage = FText::FromString("Info")
 
 TransactionData* TransactionUtils::GetTransaction(const FString& pubkey){
-    FSubscriptionData* request = new FSubscriptionData(UFRequestManager_WB::GetNextSubID());
+    TransactionData* request = new TransactionData(UFRequestManager_WB::GetNextSubID());
 	request->Body =
 		FString::Printf(TEXT(R"({"jsonrpc":"2.0","id":%d,"method":"getTransaction","params":["%s"]})")
 			,request->Id , *pubKey );
 	return request;
 }
 
-FString TransactionUtils::GetTransactionErr(TransactionData* info2read){
+FString TransactionUtils::GetTransactionErr(TransactionData* info2read)
+{
     FJsonObject* data = info2read->Response;
-	if(TSharedPtr<FJsonObject> params = data->GetObjectField("params"))
+	if(TSharedPtr<FJsonObject> params = data->GetObjectField("result"))
 	{
-		const TSharedPtr<FJsonObject> result = params->GetObjectField("result");
-		const TSharedPtr<FJsonObject> value = params->GetObjectField("value");
-		return result->GetStringField("signature");
+		const TSharedPtr<FJsonObject> result = params->GetObjectField("meta");
+		return result->GetStringField("err");
 	}
 	return "empty";
 } 
+
+int TransactionUtils::GetTransactionFee(TransactionData* info2read)
+{
+    FJsonObject* data = info2read->Response;
+	if(TSharedPtr<FJsonObject> params = data->GetObjectField("result"))
+	{
+		const TSharedPtr<FJsonObject> result = params->GetObjectField("meta");
+		return result->GetStringField("fee");
+	}
+	return NULL;
+}
+
+TransactionData* TransactionUtils::GetTransactionCount(const FString& pubkey){
+    TransactionData* request = new TransactionData(UFRequestManager_WB::GetNextSubID());
+	request->Body =
+		FString::Printf(TEXT(R"({"jsonrpc":"2.0","id":%d,"method":"getTransactionCount"})")
+			,request->Id , *pubKey );
+	return request;
+}
+
+int TransactionUtils::GetTransactionCountInfo(TransactionData* info2read)
+{
+    FJsonObject* data = info2read->Response;
+	if(TSharedPtr<FJsonObject> params = data->GetObjectField("result"))
+	{
+		return result->GetStringField("result");
+	}
+	return NULL;
+}
